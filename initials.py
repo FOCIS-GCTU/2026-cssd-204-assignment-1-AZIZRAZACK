240898704597f20b753d97e45478fd6438d12ec2
AZIZRAZACK <2425401771@live.gctu.edu.gh>
2026-05-31 09:12:29 +0000

    finished assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: AZIZ RAZACK
+# STUDENT ID:  2425401771
+# Email: 2425401771@live.gctu.edu.gh
+# Grader: Mr.Augustus Buckman
+#
+# On my honor,Mr. Augustus Buckman, I assure you that this programming assignment is entirely my own work
+# and I have not shared this code with any other student.
 
 def main():
-  pass #code goes here
+    #This function prints out the assignment header and the initials RAR using exact 10x12 dimensions.
 
-
+    print()
+    print("...RAR")
+    print()
+    print("...RRRRRRRRRRRR........AAAAAAAAAAAA........RRRRRRRRRRRR.....")
+    print("...RRRRRRRRRRRR........AAAAAAAAAAAA........RRRRRRRRRRRR.....")
+    print("...RRR......RRR........AAA......AAA........RRR......RRR.....")
+    print("...RRR......RRR........AAA......AAA........RRR......RRR.....")
+    print("...RRRRRRRRRRRR........AAAAAAAAAAAA........RRRRRRRRRRRR.....")
+    print("...RRRRRRRRRRRR........AAAAAAAAAAAA........RRRRRRRRRRRR.....")
+    print("...RRR..RRRRR..........AAA......AAA........RRR..RRRRR.......")
+    print("...RRR....RRRR.........AAA......AAA........RRR....RRRR......")
+    print("...RRR.....RRRR...**...AAA......AAA...**...RRR.....RRRR...**")
+    print("...RRR.....RRRR...**...AAA......AAA...**...RRR.....RRRR...**")
+    print()
 
 main()
+
